3dea8a5b3dfc2cb8f6f853158b7351cfc5aac72d
mayatnikov <mayatnikov@gmail.com>
2014-01-21 18:35:33 +0400

    add GPL license

--- src/home/vitaly/camel/processors/Pojo2DBObjectProcessor.java
+++ src/home/vitaly/camel/processors/Pojo2DBObjectProcessor.java
@@ -1,3 +1,11 @@
+/**************************************************************************************
+ * Copyright (C) 2014 Vitaly&Pavel team. All rights reserved.                         *
+ *                                                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the GPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+
 package home.vitaly.camel.processors;
 
 import com.mongodb.BasicDBObject;
--- src/home/vitaly/transaction/analyzer/EsperBeanInterface.java
+++ src/home/vitaly/transaction/analyzer/EsperBeanInterface.java
@@ -1,3 +1,10 @@
+/**************************************************************************************
+ * Copyright (C) 2014 Vitaly&Pavel team. All rights reserved.                         *
+ *                                                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the GPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
 package home.vitaly.transaction.analyzer;
 
 import com.espertech.esper.client.EPAdministrator;
